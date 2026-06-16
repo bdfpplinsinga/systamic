086ecb2e1003b62b1b32fb67a720bef6e19e69fa
Daniel Plinsinga <info@burotoast.nl>
2026-06-16 16:48:41 +0200

    feat: Add typography styles and font imports; update export paths in starter kit

---
+++ export/resources/js/site.js
@@ -0,0 +1,7 @@
+// https://fontsource.org/fonts/manrope/install
+// https://fontsource.org/fonts/geist/install
+
+// Supports weights 200-800
+import '@fontsource-variable/manrope/wght.css';
+// Supports weights 100-900
+import '@fontsource-variable/geist/wght.css';
