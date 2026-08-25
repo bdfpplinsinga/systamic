fe37010a58eeaf6015fb4904f04262c6ab0edbd7
bdfpplinsinga <github@toastdesign.nl>
2026-08-25 08:51:43 +0200

    updated

---
+++ export/resources/js/site.js
@@ -0,0 +1,12 @@
+// Supports weights 200-800
+import '@fontsource-variable/manrope/wght.css';
+// Supports weights 100-900
+import '@fontsource-variable/geist/wght.css';
+
+// Import tailwind elements
+import '@tailwindplus/elements';
+
+// Alphine JS
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
